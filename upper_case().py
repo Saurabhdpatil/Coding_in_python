e6467bb89b04bfc3e71e240cab9bc13d3c71cd01
Saurabh Patil <82258961+Saurabhdpatil@users.noreply.github.com>
2023-06-26 23:34:16 +0530

    Update upper_case().py

--- upper_case().py
+++ upper_case().py
@@ -1,3 +1,4 @@
+#1st question out of 150 (python pratice)
 # 1. Write a Python program to print the following string in a specific format (see the output).
 # Sample String : "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are"
 
