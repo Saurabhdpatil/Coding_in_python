ac33b0034e7792a28dc88a2aa45a223f266c81ec
Saurabh Patil <82258961+Saurabhdpatil@users.noreply.github.com>
2023-06-26 11:30:31 +0530

    Create print_function.py
    i just solved print function in hacker rank

---
+++ print_function.py
@@ -0,0 +1,47 @@
+       # MY CODE
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1,n+1):
+        print(i,end='') # here we pass end="" parameter 
+                         #The end parameter in the print function is used to add any string. 
+                          #At the end of the output of the print statement in python.
+        #By default, the print function ends with a newline. Passing the whitespace to the end parameter (end=' ') indicates that the end character has to be identified by whitespace and not a newline.
+
+
+
+
+
+
+
+
+
+#TASK
+# The included code stub will read an integer, , from STDIN.
+
+# Without using any string methods, try to print the following:
+
+
+# Note that "" represents the consecutive values in between.
+
+# Example
+
+# Print the string .
+
+# Input Format
+
+# The first line contains an integer .
+
+# Constraints
+
+
+# Output Format
+
+# Print the list of integers from  through  as a string, without spaces.
+
+# Sample Input 0
+
+# 3
+# Sample Output 0
+
+# 123
