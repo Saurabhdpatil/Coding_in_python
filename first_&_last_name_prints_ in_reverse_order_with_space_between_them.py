47b4748809eeca4ae6f12cfb8bc79dfc21edabe9
Saurabh Patil <82258961+Saurabhdpatil@users.noreply.github.com>
2023-06-27 01:13:33 +0530

    Create first_&_last_name_prints_ in_reverse_order_with_space_between_them.py
    just solve the Python program that accepts the user's first and last name and prints them in reverse order with a space between them

---
+++ first_&_last_name_prints_ in_reverse_order_with_space_between_them.py
@@ -0,0 +1,9 @@
+
+# Write a Python program that accepts the user's first and last name and prints them in reverse order with a space between them.
+
+
+
+first_name=input("enter your first name:-")
+last_name=input("enter your first name:-")
+reverse=(first_name[::-1]+" "+last_name[::-1])
+print(reverse)
