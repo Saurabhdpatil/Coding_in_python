3295581bf59ce2f575f9b738900e3e48b81f2257
Saurabh Patil <82258961+Saurabhdpatil@users.noreply.github.com>
2023-06-29 00:27:49 +0530

    Create first name last  name and middle name using format function.py

---
+++ first name last  name and middle name using format function.py
@@ -0,0 +1,4 @@
+first_name=input(str("enter your first name:-"))
+last_name=input(str("enter your last name:-"))
+middle_name=input(str("enter your middle name"))
+print("{0} {1} {2}".format(first_name,middle_name,last_name))
