5549089a492afc37cf8aacb6745e4808070e38ef
Saurabh Patil <82258961+Saurabhdpatil@users.noreply.github.com>
2023-06-30 01:11:53 +0530

    Create  program to get the volume of a sphere with radius six.py

---
+++ program to get the volume of a sphere with radius six.py
@@ -0,0 +1,7 @@
+# Write a Python program to get the volume of a sphere with radius six.
+
+r=6      #given
+pi=3.14  #we know the value of pi
+v=1.333*pi*r*r*r #4\\3 is 1.333
+
+print("volume of sphere is:-",v)
