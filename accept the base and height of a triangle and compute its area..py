7dfdcdc7866f71d1bed84b4dacefb6280c6f9ab9
Saurabh Patil <82258961+Saurabhdpatil@users.noreply.github.com>
2023-06-30 18:21:39 +0530

    Create accept the base and height of a triangle and compute its area..py

---
+++ accept the base and height of a triangle and compute its area..py
@@ -0,0 +1,6 @@
+# Write a Python program that will accept the base and height of a triangle and compute its area.
+
+base=eval(input("enter the value of base of triangle:-"))
+height=eval(input("enter the value of height of triangle:-"))
+area=(base*height)/2
+print("area of triangle is:-",area)
