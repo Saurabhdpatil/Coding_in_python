c60d3b240ae282de9e11e2524d080389a60540d7
Saurabh Patil <82258961+Saurabhdpatil@users.noreply.github.com>
2023-06-30 18:28:12 +0530

    Create program to sum three given integers. However, if two values are equal, the sum will be zero.py

---
+++ program to sum three given integers. However, if two values are equal, the sum will be zero.py
@@ -0,0 +1,14 @@
+# Write a Python program to sum three given integers. However, if two values are equal, the sum will be zero.
+
+a=int(input("enter the first integer:-"))
+b=int(input("enter the secound integer:-"))
+c=int(input("enter the third integer:-"))
+d=0
+if a==b:
+    print("sum is :-",d)
+
+elif b==c:
+    print("sum is :-",d)
+
+else:
+    print("sum of three integer is :-",a+b+c)
