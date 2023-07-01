813a77fd71fb596101018845735ce80b84e25351
Saurabh Patil <82258961+Saurabhdpatil@users.noreply.github.com>
2023-07-01 23:55:16 +0530

    Create displays your name, age, and address on three different lines.py

---
+++ displays your name, age, and address on three different lines.py
@@ -0,0 +1,8 @@
+# Write a Python program that displays your name, age, and address on three different lines.
+
+name=str(input("enter your name:-"))
+age=int(input("enter the your age:-"))
+adr=str(input("enter your address:- "))
+print("your name is:-",name)
+print("age is:-",age)
+print("& address  is:-",adr)
