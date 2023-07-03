7ec47076a390a06a3e0eba23dafcfd5d5e8a405d
Saurabh Patil <82258961+Saurabhdpatil@users.noreply.github.com>
2023-07-03 12:29:14 +0530

    Create  concatenate N strings.py

---
+++ concatenate N strings.py
@@ -0,0 +1,7 @@
+ # Write a Python program to concatenate N strings.
+
+a=str(input("enter the first string:-"))
+b=str(input("enter the secound string:-"))
+d=str(input("enter the third string:-"))
+c=a+"-"+b+"-"+d
+print(c)
