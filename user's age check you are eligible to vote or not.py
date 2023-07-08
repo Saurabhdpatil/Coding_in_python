1e1e32f67d982312a8cd11fdfaed6ff4c8ad23b6
Saurabh Patil <82258961+Saurabhdpatil@users.noreply.github.com>
2023-07-08 15:40:56 +0530

    Create user's age check you are eligible to vote or not.py

---
+++ user's age check you are eligible to vote or not.py
@@ -0,0 +1,9 @@
+# Create a program that takes a user's age and determines
+# if they are eligible to vote (considering the legal voting age as 18).
+
+age=int(input("enter your age to check you are eligible to vote or not:-"))
+
+if age >=18 :
+    print("welcome, you are eligible for voting")
+else:
+    print("sorry, you can't vote, because you are under 18...!")
