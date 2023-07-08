75c21752ac4094c686205e5a7939d17d4e4fffb2
Saurabh Patil <82258961+Saurabhdpatil@users.noreply.github.com>
2023-07-08 15:48:04 +0530

    Create converts temperature from Celsius to Fahrenheit.py

---
+++ converts temperature from Celsius to Fahrenheit.py
@@ -0,0 +1,6 @@
+# Write a program that converts temperature from Celsius to Fahrenheit.
+
+temp=eval(input("enter the temperture to convert celaius to fahrenheit:-"))
+
+a=temp *9/5 + 32
+print("Degree fahrenheit:-",a)
