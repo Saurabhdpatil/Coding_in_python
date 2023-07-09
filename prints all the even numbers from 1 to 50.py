b87d37c4528a8f5735b83706e655c43107a9e499
Saurabh Patil <82258961+Saurabhdpatil@users.noreply.github.com>
2023-07-09 16:01:55 +0530

    Create prints all the even numbers from 1 to 50.py

---
+++ prints all the even numbers from 1 to 50.py
@@ -0,0 +1,5 @@
+# Write a program that prints all the even numbers from 1 to 50.
+
+for n in range(0,51,2): # its start from 0 and stop at 50
+                        # 2 is that increment in no by 2
+    print(n)
